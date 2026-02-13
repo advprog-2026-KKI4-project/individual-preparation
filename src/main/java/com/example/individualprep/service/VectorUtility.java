f232d3403ec822f837937bfc3a28f189746e86d0
RadithyaNaufal <radithyanm@gmail.com>
2026-02-13 13:35:34 +0700

    subtract

---
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -0,0 +1,39 @@
+package com.example.individualprep.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VectorUtility {
+    
+    public double[] add(double[] v1, double[] v2) {
+        // TODO: Implement me properly!
+        return new double[] { 0.0, 0.0, 0.0 };
+    }
+
+    public double[] subtract(double[] v1, double[] v2) {
+        if (v1.length != v2.length) {
+            throw new IllegalArgumentException("Vectors must be of the same length.");
+        }
+
+        double[] result = new double[v1.length];
+        for (int i = 0; i < v1.length; i++) {
+            result[i] = v1[i] - v2[i];
+        }
+        return result;
+    }
+
+    public double[] multiply(double[] v1, int x) {
+        // TODO: Implement me properly!
+        return new double[] { 0.0, 0.0, 0.0 };
+    }
+    
+    public double dotProduct(double[] v1, double[] v2) {
+        // TODO: Implement me properly!
+        return 0.0;
+    }
+    
+    public double norm(double[] v1) {
+        // TODO: Implement me properly!
+        return 0.0;
+    }
+}
