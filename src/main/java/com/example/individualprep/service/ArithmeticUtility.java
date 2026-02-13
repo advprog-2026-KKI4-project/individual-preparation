f24dc67ef93fe6d613bc1b7e1e828fa59891a17b
RadithyaNaufal <radithyanm@gmail.com>
2026-02-13 14:26:26 +0700

    Implement arithmetic divide and resolve conflicts

--- src/main/java/com/example/individualprep/service/ArithmeticUtility.java
+++ src/main/java/com/example/individualprep/service/ArithmeticUtility.java
@@ -6,23 +6,22 @@ import org.springframework.stereotype.Service;
 public class ArithmeticUtility {
     
     public double add(double o1, double o2) {
-        // TODO: Implement me properly!
-        return 0.0;
+        return o1 + o2;
     }
 
     public double subtract(double o1, double o2) {
-        // TODO: Implement me properly!
-        return 0.0;
+        return o1 - o2;
     }
 
     public double multiply(double o1, double o2) {
-        // TODO: Implement me properly!
-        return 0.0;
+        return o1 * o2;
     }
 
     public double divide(double o1, double o2) {
-        // TODO: Implement me properly!
-        return 0.0;
+        if (o2 == 0.0) {
+            throw new ArithmeticException("Cannot divide by zero.");
+        }
+        return o1 / o2;
     }
 
     public double exponent(double o1, int n) {
